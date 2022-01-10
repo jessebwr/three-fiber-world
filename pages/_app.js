d1830e4d7f17c2f614c18b7bbe837cbad21cc9ab
Jesse Berlin Watts-Russell <jwattsrussell@flatiron.com>
2022-01-10 15:09:38 -0500

    earth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { Global, css } from "@emotion/react";
+
+const globalCss = css`
+  html,
+  body,
+  #__next {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={globalCss} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
